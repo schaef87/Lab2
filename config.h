9825f1a5064379c101a0b93b426398e63d402285
Justin Schaefer <jds_06@hotmail.com>
2015-02-09 13:07:06 -0600

    Starter

---
+++ config.h
@@ -0,0 +1,10 @@
+#define LAB2_TEST1	0		// Date constructors
+#define LAB2_TEST2	0		// Date getters
+#define LAB2_TEST3	0		// isLeapYear
+#define LAB2_TEST4	0		// daysInMonth
+#define LAB2_TEST5	0		// Date operator<<
+#define LAB2_TEST6	0		// BlogEntry constructors
+#define LAB2_TEST7	0		// BlogEntry getters/setters
+#define LAB2_TEST8	0		// Exercise 1 - printHTML
+#define LAB2_TEST9	0		// Exercise 2 - getDayOfWeek
+#define LAB2_TEST10	0		// Exercise 3 - Date relational operators
